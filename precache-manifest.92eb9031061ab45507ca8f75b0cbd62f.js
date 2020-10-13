bb5244915973ad5bdf5f852f13da7a8f994b8737
Kareem Ismail <KareemTahaIsmail@gmail.com>
2020-10-12 20:34:53 -0400

    Updates

---
+++ precache-manifest.92eb9031061ab45507ca8f75b0cbd62f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "63222e7e27821b947d13",
+    "url": "/SpaceXFactsSince2006/static/css/main.6cdad126.chunk.css"
+  },
+  {
+    "revision": "63222e7e27821b947d13",
+    "url": "/SpaceXFactsSince2006/static/js/main.63222e7e.chunk.js"
+  },
+  {
+    "revision": "1192ab50fc54491d547d",
+    "url": "/SpaceXFactsSince2006/static/js/1.1192ab50.chunk.js"
+  },
+  {
+    "revision": "6c4fd14e86812ed0eecb",
+    "url": "/SpaceXFactsSince2006/static/js/runtime~main.6c4fd14e.js"
+  },
+  {
+    "revision": "dcc9a6865a41e1a17bac365159f11afc",
+    "url": "/SpaceXFactsSince2006/static/media/space.dcc9a686.jpg"
+  },
+  {
+    "revision": "1e78fa0fa003019d3c5dfd218b2eb240",
+    "url": "/SpaceXFactsSince2006/index.html"
+  }
+];
